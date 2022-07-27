129645585ebfd19ff6ca3439c04709b95367f896
charlieallen <ceallen1007@gmail.com>
2022-07-26 19:52:57 -0500

    commit tree 62b8b7c5f0fd19324054c4ee9e6c3b40d48d336a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const newHeader = document.createElement("h1")
+
+//document.body.append(newHeader)
+document.querySelector("#main").remove()
+newHeader.id = "victory"
+newHeader.textContent = "CHARLIE ALLEN is the champion"
